9d269d7e180ec893fd497db8483775d1196116d6
ilic993 <34837063+ilic993@users.noreply.github.com>
2022-01-27 20:10:19 +0100

    Adding a form submit event

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
-console.log("Hello universe :)");
+import PublitioAPI from '../node_modules/publitio_js_sdk/build/publitio-api.min.js'
+
+const FORM = document.querySelector('#upload-form')
+
+FORM.addEventListener("submit", (e) => submitForm(e))
+
+function submitForm(event){
+    event.preventDefault()
+    console.log("Form submitted")
+}
